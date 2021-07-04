ec1f63ba3ec91b834bd6589c1ede30165f8af757
Diego Andersen <diego.andersen@protonmail.com>
2021-07-04 12:06:03 +0200

    Re-wrote vol control script to use Pulse instead of ALSA, added volume ceiling and floor, added empty volume markers to notification bar.

--- scripts/volume_control.sh
+++ scripts/volume_control.sh
@@ -1,28 +1,58 @@
 #!/usr/bin/env zsh
 
-# Script modified from this amazing gist:
+# Script modified from this gist:
 # https://gist.github.com/Blaradox/030f06d165a82583ae817ee954438f2e
+#
+# Entirely uses Pulse, no ALSA in sight.
 
 # TODO: Use .ini/.cfg file so that paths to icons aren't hard-coded
 
 function get_volume {
-  amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+    pacmd list-sinks | grep volume | head -n 1 | cut -d: -f3 | cut -d"/" -f 2 | tr -d % | xargs
 }
 
 function is_mute {
-  amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+    pacmd list-sinks | grep muted | cut -d: -f2 | xargs
+}
+
+function raise_vol {
+    currentVol=$(get_volume)
+
+    if [ $currentVol = 120 ]; then
+        newVol=120;
+    else
+        newVol="$(( $currentVol + 10 ))";
+    fi
+
+    pactl set-sink-volume 0 $newVol% > /dev/null
+}
+
+function lower_vol {
+    currentVol=get_volume
+
+    if [ $currentVol = 0 ]; then
+        newVol=0;
+    else
+        newVol="$(( $currentVol - 10 ))";
+    fi
+
+    pactl set-sink-volume 0 $newVol% > /dev/null
 }
 
 function send_notification {
   iconSound=~/.config/dunst/icons/volume_dark_blue.png
   iconMuted=~/.config/dunst/icons/muted_grey.png
 
-  # Make the bar with the special character ─ (it's not dash -)
-  # https://en.wikipedia.org/wiki/Box-drawing_character
+  # TODO: Maybe don't hard-code maxvol 120 and make it a variable
   volume=$(get_volume)
-  bar=$(seq --separator="▐" 0 "$((volume / 10))" | sed 's/[0-9]//g')
+  vol_full="$(( volume / 10 ))"
+  vol_empty="$(( 12 - volume / 10 ))"
 
-  if is_mute ; then
+  bar_full=$(seq --separator="■" 0 "$vol_full" | sed 's/[0-9]//g')
+  bar_empty=$(seq --separator="□" 0 "$vol_empty" | sed 's/[0-9]//g')
+  bar=$bar_full$bar_empty
+
+  if [ $(is_mute) = "yes" ]; then
     dunstify -a volume -i $iconMuted -r 2593 -u low "    $bar"
   else
     dunstify -a volume -i $iconSound -r 2593 -u normal "    $bar"
@@ -31,19 +61,17 @@ function send_notification {
 
 case $1 in
   up)
-    # set the volume on (if it was muted)
+    # Always un-mute when raising volume
     pactl set-sink-mute 0 0
-    # up the volume (+ 5%)
-    pactl set-sink-volume 0 +10%
+    raise_vol
     send_notification
     ;;
   down)
-    pactl set-sink-volume 0 -10%
+    lower_vol
     send_notification
     ;;
   mute)
-    # toggle mute
-    pactl set-sink-mute 0 toggle
+    pactl set-sink-mute 0 toggle > /dev/null
     send_notification
     ;;
 esac
